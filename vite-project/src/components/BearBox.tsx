0444e15c481e32728462dde0b835ee03fe2fc2fc
Martin Odongo <martinwere14@gmail.com>
2024-11-15 09:28:16 +0300

    learning state management using zustand

--- vite-project/src/components/BearBox.tsx
+++ vite-project/src/components/BearBox.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useBearStore } from '../stores/store'
 
 const BearBox = () => {
+  const bears = useBearStore(state => state.bears)
+  const increasePopulation = useBearStore(state=> state.increasePopulation)
+  const removeAllBears = useBearStore(state=> state.removeAllBears)
   return (
     <>
      <div className='box'>
       <h1>Bear Box</h1>
+      <p>Bear:{bears}</p>
     </div>
     <div>
-      <button>add bear</button>
-      <button>remove bear</button>
+      <button onClick={increasePopulation}>add bear</button>
+      <button onClick={removeAllBears}>remove all bears</button>
     </div>
     </>
    
